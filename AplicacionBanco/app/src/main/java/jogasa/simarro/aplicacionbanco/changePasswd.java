424ca9632a575f7a2ac9ce23ae16c8c543332767
Aspectz <josepgarzo7@gmail.com>
2020-10-18 22:14:28 +0200

    App

--- AplicacionBanco/app/src/main/java/jogasa/simarro/aplicacionbanco/changePasswd.java
+++ AplicacionBanco/app/src/main/java/jogasa/simarro/aplicacionbanco/changePasswd.java
@@ -5,24 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+public class changePasswd extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_change_passwd);
 
-        ImageButton btnAcceder=(ImageButton)findViewById(R.id.btnAcceder);
+        Button btnAcceder=(Button)findViewById(R.id.btnchangecontrasena);
 
 
         btnAcceder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this,mainAppActivity.class);
+                Intent intent=new Intent(changePasswd.this,mainAppActivity.class);
                 startActivity(intent);
+                Toast.makeText(changePasswd.this, getText(R.string.contraseñaActualizada), Toast.LENGTH_SHORT).show();
             }
         });
     }
--- AplicacionBanco/app/src/main/java/jogasa/simarro/aplicacionbanco/mainAppActivity.java
+++ AplicacionBanco/app/src/main/java/jogasa/simarro/aplicacionbanco/mainAppActivity.java
@@ -2,7 +2,11 @@ package jogasa.simarro.aplicacionbanco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class mainAppActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class mainAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
+
+        ImageButton btnAcceder=(ImageButton)findViewById(R.id.btnContrasena);
+        Button btnSalir=(Button)findViewById(R.id.btnSalir);
+
+        btnAcceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(mainAppActivity.this,changePasswd.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(mainAppActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
